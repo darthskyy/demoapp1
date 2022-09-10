acb78fcdeea7241ad3a08fe47cfad176769c643a
Simbarashe Mawere <20simbarashem@gmail.com>
2022-09-10 15:16:18 +0200

    Back button function overrided to get existing letters from a queue then go back to overview on the last thing on queue.
    Back button pressed on letter Z is not working; investigation necessary

--- app/src/main/java/com/example/demoapp1/LetterActivity.kt
+++ app/src/main/java/com/example/demoapp1/LetterActivity.kt
@@ -1,8 +1,10 @@
 package com.example.demoapp1
 
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import android.widget.Button
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import pl.droidsonroids.gif.GifImageView
 
@@ -23,7 +25,7 @@ class LetterActivity : AppCompatActivity() {
 //            isEnabled = (currentLetter!=25)
 ////            isClickable = (currentLetter!=25)
 //        }
-
+    var queue = mutableListOf<Any>()
     val letters2 = arrayOf(
         R.drawable.a,
         R.drawable.b,
@@ -53,30 +55,65 @@ class LetterActivity : AppCompatActivity() {
         R.drawable.z,
     )
 
+    override fun onBackPressed() {
+        if (queue.isEmpty()) {
+            super.onBackPressed()
+        }
+        else {
+            if (queue.size==1) {
+                val img = findViewById<GifImageView>(R.id.currentLetter).apply {
+                    setImageResource(queue[0] as Int)
+                }
+                currentLetter = letters2.indexOf(queue[0] as Int)
+                queue = mutableListOf<Any>()
+                Toast.makeText(applicationContext, "In list: "+ queue, Toast.LENGTH_LONG).show()
+            }
+            else {
+                val img = findViewById<GifImageView>(R.id.currentLetter).apply {
+                    setImageResource(queue[queue.size-1] as Int)
+                }
+                currentLetter = letters2.indexOf(queue[queue.size-1] as Int)
+                queue = queue.dropLast(1) as MutableList<Any>
+                Toast.makeText(applicationContext, "In list: "+ queue, Toast.LENGTH_LONG).show()
+            }
+        }
+    }
+
+    override fun onRestart() {
+        queue = mutableListOf<Any>()
+        super.onRestart()
+    }
     fun goToA(view: View) {
+        queue.add(R.drawable.a)
         val img = findViewById<GifImageView>(R.id.currentLetter).apply {
             setImageResource(R.drawable.a)
             currentLetter=0
+            Toast.makeText(applicationContext, "In list: "+ queue, Toast.LENGTH_LONG).show()
         }
     }
 
     fun goToZ(view: View) {
+        queue.add(R.drawable.z)
         val img = findViewById<GifImageView>(R.id.currentLetter).apply {
             setImageResource(R.drawable.z)
             currentLetter=25
+            Toast.makeText(applicationContext, "In list: "+ queue, Toast.LENGTH_LONG).show()
         }
-
     }
 
     fun goToNext(view: View) {
+        queue.add(letters2[currentLetter])
         val img = findViewById<GifImageView>(R.id.currentLetter).apply {
             setImageResource(letters2[++currentLetter])
+            Toast.makeText(applicationContext, "In list: "+ queue, Toast.LENGTH_LONG).show()
         }
     }
 
     fun goToPrev(view: View) {
+        queue.add(letters2[currentLetter])
         val img = findViewById<GifImageView>(R.id.currentLetter).apply {
             setImageResource(letters2[--currentLetter])
+            Toast.makeText(applicationContext, "In list: "+ queue, Toast.LENGTH_LONG).show()
         }
     }
 }
